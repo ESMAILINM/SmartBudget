9f879ad91bb15247769223afdb13e87aa84d7990
ESMAILINM <esmailinmartinez05@gmail.com>
2025-11-19 14:52:13 -0400

    Agregar interfaz SmartBudgetApi para la gestión de usuarios

---
+++ app/src/main/java/edu/ucne/smartbudget/data/remote/SmartBudgetApi.kt
@@ -0,0 +1,24 @@
+package edu.ucne.smartbudget.data.remote
+
+import edu.ucne.smartbudget.data.remote.dto.UsuariosDto
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface SmartBudgetApi {
+
+    @GET("api/Usuarios")
+    suspend fun getUsuarios(): List<UsuariosDto>
+
+    @GET ("api/Usuarios/{id}")
+    suspend fun getUsuario(@Path("id") id: Int): UsuariosDto?
+
+    @PUT ("api/Usuarios/{id}")
+    suspend fun updateUsuario(@Path("id") id: Int, @Body usuarios: UsuariosDto ): Response<Unit>
+
+    @POST ("api/Usuarios")
+    suspend fun createUsuario(@Body usuarios: UsuariosDto ): Response<UsuariosDto>
+}
