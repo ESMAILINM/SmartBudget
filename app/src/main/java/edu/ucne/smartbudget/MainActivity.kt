5a0497458250bd0bb210a0ac9d8616b172631db1
ESMAILINM <esmailinmartinez05@gmail.com>
2025-11-19 15:06:42 -0400

    Agregar navegación y configuración de tema en MainActivity

--- app/src/main/java/edu/ucne/smartbudget/MainActivity.kt
+++ app/src/main/java/edu/ucne/smartbudget/MainActivity.kt
@@ -3,45 +3,29 @@ package edu.ucne.smartbudget
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import androidx.navigation.compose.rememberNavController
+import edu.ucne.smartbudget.presentation.navegation.SmartBudgetNavHost
 import edu.ucne.smartbudget.ui.theme.SmartBudgetTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             SmartBudgetTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    SmartBudgetNavHost(navController = navController)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartBudgetTheme {
-        Greeting("Android")
     }
 }
