7cc02ca8ee84ac83e86ca1d5401d5baba07b23c3
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-05 03:03:23 -0400

    Agregar inyección de dependencias para repositorios de ingresos, gastos, categorías y metas

--- app/src/main/java/edu/ucne/smartbudget/di/RepositoryModule.kt
+++ app/src/main/java/edu/ucne/smartbudget/di/RepositoryModule.kt
@@ -4,7 +4,15 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import edu.ucne.smartbudget.data.remote.repository.CategoriasRepositoryImpl
+import edu.ucne.smartbudget.data.remote.repository.GastosRepositoryImpl
+import edu.ucne.smartbudget.data.remote.repository.IngresosRepositoryImpl
+import edu.ucne.smartbudget.data.remote.repository.MetasRepositoryImpl
 import edu.ucne.smartbudget.data.remote.repository.UsuarioRepositoryImpl
+import edu.ucne.smartbudget.domain.repository.CategoriaRepository
+import edu.ucne.smartbudget.domain.repository.GastosRepository
+import edu.ucne.smartbudget.domain.repository.IngresoRepository
+import edu.ucne.smartbudget.domain.repository.MetasRepository
 import edu.ucne.smartbudget.domain.repository.UsuarioRepository
 import javax.inject.Singleton
 
@@ -18,4 +26,28 @@ abstract class RepositoryModule {
     abstract fun bindUserRepository(
         usuarioRepositoryImpl: UsuarioRepositoryImpl
     ): UsuarioRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindIngresoRepository(
+        ingresoRepositoryImpl: IngresosRepositoryImpl
+    ): IngresoRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindCategoriaRepository(
+        categoriasRepositoryImpl: CategoriasRepositoryImpl
+    ): CategoriaRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindGastoRepository(
+        gastosRepositoryImpl: GastosRepositoryImpl
+    ): GastosRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindMetasRepository(
+        metasRepositoryImpl: MetasRepositoryImpl
+    ): MetasRepository
 }
