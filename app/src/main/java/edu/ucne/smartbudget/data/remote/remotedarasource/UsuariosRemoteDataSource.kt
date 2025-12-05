56a28b17cf656aec0c71443e542e9e379266e48b
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-05 03:36:22 -0400

    Agregar UsuariosRemoteDataSource

---
+++ app/src/main/java/edu/ucne/smartbudget/data/remote/remotedarasource/UsuariosRemoteDataSource.kt
@@ -0,0 +1,51 @@
+package edu.ucne.smartbudget.data.remote.remotedatasource
+
+import edu.ucne.smartbudget.data.remote.Resource
+import edu.ucne.smartbudget.data.remote.SmartBudgetApi
+import edu.ucne.smartbudget.data.remote.dto.usuariosdto.UsuarioRequest
+import edu.ucne.smartbudget.data.remote.dto.usuariosdto.UsuarioResponse
+import javax.inject.Inject
+
+class UsuariosRemoteDataSource @Inject constructor(
+    private val api: SmartBudgetApi
+) {
+    suspend fun createUsuario(request: UsuarioRequest): Resource<UsuarioResponse> {
+        return try {
+            val response = api.createUsuario(request)
+            if (response.isSuccessful) {
+                response.body()?.let { Resource.Success(it) }
+                    ?: Resource.Error("Respuesta vacía del servidor")
+            } else {
+                Resource.Error("HTTP ${response.code()} ${response.message()}")
+            }
+        } catch (e: Exception) {
+            Resource.Error(e.localizedMessage ?: "Error de red")
+        }
+    }
+
+    suspend fun updateUsuario(id: Int, request: UsuarioRequest): Resource<Unit> {
+        return try {
+            val response = api.updateUsuario(id, request)
+            if (response.isSuccessful) {
+                Resource.Success(Unit)
+            } else {
+                Resource.Error("HTTP ${response.code()} ${response.message()}")
+            }
+        } catch (e: Exception) {
+            Resource.Error(e.localizedMessage ?: "Error de red")
+        }
+    }
+
+    suspend fun deleteUsuario(id: Int): Resource<Unit> {
+        return try {
+            val response = api.deleteUsuario(id)
+            if (response.isSuccessful) {
+                Resource.Success(Unit)
+            } else {
+                Resource.Error("HTTP ${response.code()} ${response.message()}")
+            }
+        } catch (e: Exception) {
+            Resource.Error(e.localizedMessage ?: "Error de red")
+        }
+    }
+}
