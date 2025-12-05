ccdc133915f11f1d8b37201f5ce0a46918edf79f
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-05 01:31:24 -0400

    Agregar mapeadores DTOs, modelos de dominio y entidades de usuario.

---
+++ app/src/main/java/edu/ucne/smartbudget/data/mapper/UsuariosMapper.kt
@@ -0,0 +1,52 @@
+package edu.ucne.smartbudget.data.mapper
+
+import edu.ucne.smartbudget.data.local.entities.UsuariosEntity
+import edu.ucne.smartbudget.data.remote.dto.usuariosdto.UsuarioRequest
+import edu.ucne.smartbudget.data.remote.dto.usuariosdto.UsuarioResponse
+import edu.ucne.smartbudget.domain.model.Usuarios
+import java.util.UUID
+
+fun UsuarioRequest.toDomain(): Usuarios =
+    Usuarios(
+        usuarioId = UUID.randomUUID().toString(),
+        remoteId = null,
+        userName = userName,
+        password = password
+    )
+
+fun Usuarios.toRequest(): UsuarioRequest =
+    UsuarioRequest(
+        userName = userName,
+        password = password
+    )
+
+fun Usuarios.toEntity(
+    isPendingCreate: Boolean = false,
+    isPendingUpdate: Boolean = false,
+    isPendingDelete: Boolean = false
+): UsuariosEntity =
+    UsuariosEntity(
+        usuarioId = usuarioId,
+        remoteId = remoteId,
+        userName = userName,
+        password = password,
+        isPendingCreate = isPendingCreate,
+        isPendingUpdate = isPendingUpdate,
+        isPendingDelete = isPendingDelete
+    )
+
+fun UsuariosEntity.toDomain(): Usuarios =
+    Usuarios(
+        usuarioId = usuarioId,
+        remoteId = remoteId,
+        userName = userName,
+        password = password
+    )
+
+fun UsuarioResponse.toDomain(): Usuarios =
+    Usuarios(
+        usuarioId = UUID.randomUUID().toString(),
+        remoteId = usuarioId,
+        userName = userName,
+        password = ""
+    )
