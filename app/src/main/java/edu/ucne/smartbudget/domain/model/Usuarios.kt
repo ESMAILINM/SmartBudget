3915a6cd780a57be1485b17c2d6a5a5fc54ee931
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-04 22:40:15 -0400

    Actualizar modelo Usuarios con UUID y campo remoteId

--- app/src/main/java/edu/ucne/smartbudget/domain/model/Usuarios.kt
+++ app/src/main/java/edu/ucne/smartbudget/domain/model/Usuarios.kt
@@ -1,7 +1,10 @@
 package edu.ucne.smartbudget.domain.model
 
+import java.util.UUID
+
 data class Usuarios (
-    val usuarioId: Int?,
+    val usuarioId: String = UUID.randomUUID().toString(),
+    val remoteId: Int? = null,
     val userName: String,
-    val password: String
+    val password: String,
 )
