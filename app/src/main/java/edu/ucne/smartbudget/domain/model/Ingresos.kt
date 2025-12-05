4ac4c28d96c4d5ef1e28bea499e9d35323a4e7f4
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-04 23:59:27 -0400

    Agregar modelo de dominio Ingresos

---
+++ app/src/main/java/edu/ucne/smartbudget/domain/model/Ingresos.kt
@@ -0,0 +1,13 @@
+package edu.ucne.smartbudget.domain.model
+
+import java.util.UUID
+
+data class Ingresos(
+    val ingresoId: String = UUID.randomUUID().toString(),
+    val remoteId: Int? = null,
+    val monto: Double,
+    val fecha: String,
+    val descripcion: String? = null,
+    val categoriaId: String,
+    val usuarioId: String,
+)
