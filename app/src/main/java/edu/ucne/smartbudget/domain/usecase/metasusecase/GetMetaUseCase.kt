f2950f31682a51074098a5457b7731370ec116f6
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-05 13:52:51 -0400

    feat(metas): ✨ agregar caso de uso GetMetaUseCase

---
+++ app/src/main/java/edu/ucne/smartbudget/domain/usecase/metasusecase/GetMetaUseCase.kt
@@ -0,0 +1,15 @@
+package edu.ucne.smartbudget.domain.usecase.metasusecase
+
+import edu.ucne.smartbudget.data.remote.Resource
+import edu.ucne.smartbudget.domain.model.Metas
+import edu.ucne.smartbudget.domain.repository.MetasRepository
+import javax.inject.Inject
+
+class GetMetaUseCase @Inject constructor(
+    private val metasRepository: MetasRepository
+) {
+    suspend operator fun invoke(id: String): Resource<Metas?> {
+        return metasRepository.getMeta(id)
+    }
+}
+
