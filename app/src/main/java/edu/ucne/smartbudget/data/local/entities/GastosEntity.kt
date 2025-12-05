dff51445eba16e984905e523710350acfc277822
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-04 23:31:29 -0400

    Agregar modelo de dominio Gastos

---
+++ app/src/main/java/edu/ucne/smartbudget/data/local/entities/GastosEntity.kt
@@ -0,0 +1,20 @@
+package edu.ucne.smartbudget.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity(tableName = "gastos")
+data class GastosEntity(
+    @PrimaryKey val gastoId: String = UUID.randomUUID().toString(),
+    val remoteId: Int? = null,
+    val monto: Double,
+    val fecha: String,
+    val descripcion: String? = null,
+    val categoriaId: String,
+    val usuarioId: String,
+    val isPendingCreate: Boolean = false,
+    val isPendingUpdate: Boolean = false,
+    val isPendingDelete: Boolean = false
+
+)
