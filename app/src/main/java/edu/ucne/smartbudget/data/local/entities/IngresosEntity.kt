82c72ae30e8fef1f4319717ca11137b07ea36513
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-05 01:14:15 -0400

    Agregar entidad IngresosEntity

---
+++ app/src/main/java/edu/ucne/smartbudget/data/local/entities/IngresosEntity.kt
@@ -0,0 +1,21 @@
+package edu.ucne.smartbudget.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+import edu.ucne.smartbudget.domain.model.Categorias
+
+@Entity(tableName = "ingresos")
+data class IngresosEntity(
+    @PrimaryKey val ingresoId: String = UUID.randomUUID().toString(),
+    val remoteId: Int? = null,
+    val monto: Double,
+    val fecha: String,
+    val descripcion: String,
+    val categoriaId: String,
+    val usuarioId: String,
+    val isPendingCreate: Boolean = false,
+    val isPendingUpdate: Boolean = false,
+    val isPendingDelete: Boolean = false
+
+)
