f3e2270c17eb893a08bb0ee56e35c873bd3be607
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-05 02:44:46 -0400

    Configurar WorkManager con inyección de dependencias en la clase Application

--- app/src/main/java/edu/ucne/smartbudget/SmartBudget.kt
+++ app/src/main/java/edu/ucne/smartbudget/SmartBudget.kt
@@ -2,7 +2,18 @@ package edu.ucne.smartbudget
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import androidx.work.Configuration
+import edu.ucne.smartbudget.data.MyWorkerFactory
+import jakarta.inject.Inject
 
 @HiltAndroidApp
-class SmartBudget : Application() {
+class SmartBudget : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: MyWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
