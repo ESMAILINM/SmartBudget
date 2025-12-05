274273097ca41e74dc95b444c3bcd2f3629c4ad2
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-04 23:53:54 -0400

    Agregar entidad categorías

---
+++ app/src/main/java/edu/ucne/smartbudget/data/local/entities/CategoriasEntity.kt
@@ -0,0 +1,16 @@
+package edu.ucne.smartbudget.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity(tableName = "categorias")
+data class CategoriasEntity(
+    @PrimaryKey val categoriaId: String = UUID.randomUUID().toString(),
+    val remoteId: Int? = null,
+    val nombre: String = "",
+    val tipoId: Int,
+    val isPendingCreate: Boolean = false,
+    val isPendingUpdate: Boolean = false,
+    val isPendingDelete: Boolean = false
+)
