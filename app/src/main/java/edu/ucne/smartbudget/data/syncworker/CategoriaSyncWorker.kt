c1e9ead2ffc63d6109e82dffb2516359494f82dd
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-05 02:48:05 -0400

    Agregar clase CategoriaSyncWorker para la sincronización de categorías en segundo plano

---
+++ app/src/main/java/edu/ucne/smartbudget/data/syncworker/CategoriaSyncWorker.kt
@@ -0,0 +1,33 @@
+package edu.ucne.smartbudget.data.syncworker
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import edu.ucne.smartbudget.data.remote.Resource
+import edu.ucne.smartbudget.domain.repository.CategoriaRepository
+
+@HiltWorker
+class CategoriaSyncWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted workerParams: WorkerParameters,
+    private val categoriaRepository: CategoriaRepository
+) : CoroutineWorker(context, workerParams) {
+
+    override suspend fun doWork(): Result {
+        when (categoriaRepository.postPendingCategorias()) {
+            is Resource.Error -> return Result.retry()
+        }
+
+        when (categoriaRepository.postPendingUpdates()) {
+            is Resource.Error -> return Result.retry()
+        }
+
+        return when (categoriaRepository.postPendingDeletes()) {
+            is Resource.Error -> Result.retry()
+            else -> Result.success()
+        }
+    }
+}
