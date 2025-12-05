e215654ced78e823af7fab0211ca611329b2908a
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-05 01:32:19 -0400

    Agregar CategoriasMapper

---
+++ app/src/main/java/edu/ucne/smartbudget/data/mapper/CategoriasMapper.kt
@@ -0,0 +1,51 @@
+package edu.ucne.smartbudget.data.mapper
+
+import edu.ucne.smartbudget.data.local.entities.CategoriasEntity
+import edu.ucne.smartbudget.data.remote.dto.categoriasdto.CategoriaRequest
+import edu.ucne.smartbudget.data.remote.dto.categoriasdto.CategoriaResponse
+import edu.ucne.smartbudget.domain.model.Categorias
+import java.util.UUID
+
+fun CategoriasEntity.toDomain(): Categorias =
+    Categorias(
+        categoriaId = categoriaId,
+        remoteId = remoteId,
+        nombre = nombre,
+        tipoId = tipoId
+    )
+
+fun Categorias.toEntity(): CategoriasEntity =
+    CategoriasEntity(
+        categoriaId = categoriaId,
+        remoteId = remoteId,
+        nombre = nombre,
+        tipoId = tipoId,
+        isPendingCreate = false,
+        isPendingUpdate = false,
+        isPendingDelete = false
+    )
+
+fun Categorias.toRequest(): CategoriaRequest =
+    CategoriaRequest(
+        nombre = nombre,
+        tipoId = tipoId
+    )
+
+fun CategoriaResponse.toEntity(currentLocalId: String? = null): CategoriasEntity =
+    CategoriasEntity(
+        categoriaId = currentLocalId ?: UUID.randomUUID().toString(),
+        remoteId = categoriaId,
+        nombre = nombre,
+        tipoId = tipoId,
+        isPendingCreate = false,
+        isPendingUpdate = false,
+        isPendingDelete = false
+    )
+
+fun CategoriaResponse.toDomain(currentLocalId: String? = null): Categorias =
+    Categorias(
+        categoriaId = currentLocalId ?: UUID.randomUUID().toString(),
+        remoteId = categoriaId,
+        nombre = nombre,
+        tipoId = tipoId
+    )
