f82d1b35fe4d2d1d34ff87c1b321128b350789a6
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-04 23:49:38 -0400

    Agregar interfaz UsuarioDao con métodos CRUD y consultas para sincronización

---
+++ app/src/main/java/edu/ucne/smartbudget/data/local/dao/UsuarioDao.kt
@@ -0,0 +1,34 @@
+package edu.ucne.smartbudget.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.smartbudget.data.local.entities.UsuariosEntity
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface UsuarioDao {
+
+    @Query("SELECT * FROM usuarios")
+    fun observeUsuarios(): Flow<List<UsuariosEntity>>
+
+    @Query("SELECT * FROM usuarios WHERE usuarioId = :id")
+    suspend fun getUsuario(id: String): UsuariosEntity?
+
+    @Upsert
+    suspend fun upsertUsuario(usuario: UsuariosEntity)
+
+    @Query("DELETE FROM usuarios WHERE usuarioId = :id")
+    suspend fun deleteUsuario(id: String)
+
+    @Query("SELECT * FROM usuarios WHERE isPendingCreate = 1")
+    suspend fun getPendingCreateUsuarios(): List<UsuariosEntity>
+
+    @Query("SELECT * FROM usuarios WHERE isPendingUpdate = 1")
+    suspend fun getPendingUpdate(): List<UsuariosEntity>
+
+    @Query("SELECT * FROM usuarios WHERE isPendingDelete = 1")
+    suspend fun getPendingDelete(): List<UsuariosEntity>
+}
+
