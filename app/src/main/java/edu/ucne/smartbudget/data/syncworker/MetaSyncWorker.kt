b890f84b1da5b592e6c37ae349389cecefd1b83c
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-05 02:49:55 -0400

    Agregar MetaSyncWorker

---
+++ app/src/main/java/edu/ucne/smartbudget/data/syncworker/MetaSyncWorker.kt
@@ -0,0 +1,36 @@
+package edu.ucne.smartbudget.data.syncworker
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.WorkerParameters
+import androidx.work.CoroutineWorker
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import edu.ucne.smartbudget.data.remote.Resource
+import edu.ucne.smartbudget.domain.repository.MetasRepository
+
+@HiltWorker
+class MetaSyncWorker @AssistedInject constructor(
+    @Assisted appContext: Context,
+    @Assisted workerParams: WorkerParameters,
+    private val metasRepository: MetasRepository
+) : CoroutineWorker(appContext, workerParams) {
+
+    override suspend fun doWork(): Result {
+        when (metasRepository.postPendingMetas()) {
+            is Resource.Error -> return Result.retry()
+            else -> { }
+        }
+
+        when (metasRepository.postPendingUpdates()) {
+            is Resource.Error -> return Result.retry()
+            else -> { }
+        }
+
+        return when (metasRepository.postPendingDeletes()) {
+            is Resource.Success -> Result.success()
+            is Resource.Error -> Result.retry()
+            else -> Result.failure()
+        }
+    }
+}
