50331e1a5d81d417c177424b54c95d92a95d69d4
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-05 00:22:08 -0400

    Agregar modelo de datos Imagenes

---
+++ app/src/main/java/edu/ucne/smartbudget/domain/model/Imagenes.kt
@@ -0,0 +1,10 @@
+package edu.ucne.smartbudget.domain.model
+
+import java.util.UUID
+
+data class Imagenes(
+    val imagenId: String = UUID.randomUUID().toString(),
+    val remoteId: Int? = null,
+    val metaId: String,
+    val url: String ,
+)
