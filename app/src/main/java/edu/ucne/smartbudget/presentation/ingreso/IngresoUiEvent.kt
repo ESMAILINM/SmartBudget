fdecacad57b996eeab7c492c1b97feac791bfa35
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-10 13:23:15 -0400

    feat(ingreso): ✨ agregar clase sellada IngresoUiEvent para manejar eventos de la UI relacionados con ingresos

---
+++ app/src/main/java/edu/ucne/smartbudget/presentation/ingreso/IngresoUiEvent.kt
@@ -0,0 +1,25 @@
+package edu.ucne.smartbudget.presentation.ingreso
+
+import edu.ucne.smartbudget.domain.model.Ingresos
+
+sealed class IngresoUiEvent {
+    data class DescripcionChanged(val descripcion: String) : IngresoUiEvent()
+
+    data class FechaChanged(val fecha: String) : IngresoUiEvent()
+
+    data class CategoriaIdChanged(val categoriaId: String) : IngresoUiEvent()
+
+    data class MontoChanged(val monto: String) : IngresoUiEvent()
+
+    data class Load(val usuarioId: String) : IngresoUiEvent()
+
+    object Save : IngresoUiEvent()
+
+    object ShowDialog : IngresoUiEvent()
+
+    object HideDialog : IngresoUiEvent()
+
+    data class Delete(val id: String) : IngresoUiEvent()
+
+    data class Edit(val ingreso: Ingresos) : IngresoUiEvent()
+}
