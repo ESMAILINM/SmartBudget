0e713f1132ad92c5a093acd6a4b3e7a98480973b
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-10 14:07:32 -0400

    feat(financialdata): ✨ agregar clase de datos FinancialData para gestionar información financiera

---
+++ app/src/main/java/edu/ucne/smartbudget/presentation/perfil/components/FinancialData.kt
@@ -0,0 +1,8 @@
+package edu.ucne.smartbudget.presentation.perfil.components
+
+ data class FinancialData(
+    val balance: Double,
+    val metaName: String,
+    val metaGoal: Double,
+    val progress: Float
+)
