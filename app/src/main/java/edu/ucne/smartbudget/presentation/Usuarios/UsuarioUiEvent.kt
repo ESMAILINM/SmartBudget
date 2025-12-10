850ffeac4b0b2b7dc00d5c182eb6315616df9529
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-10 13:06:10 -0400

    refactor(usuario): ♻️ mejorar eventos de UI de usuario y agregar nuevos estados

--- app/src/main/java/edu/ucne/smartbudget/presentation/Usuarios/UsuarioUiEvent.kt
+++ app/src/main/java/edu/ucne/smartbudget/presentation/Usuarios/UsuarioUiEvent.kt
@@ -1,14 +1,20 @@
 package edu.ucne.smartbudget.presentation.Usuarios
 
-sealed class UsuarioUiEvent {
-    data class userNameChanged(val usuario: String) : UsuarioUiEvent()
-    data class passwordChanged(val password: String) : UsuarioUiEvent()
-    data class load(val usuarioId: Int) : UsuarioUiEvent()
-    object Save : UsuarioUiEvent()
-    object showDialog : UsuarioUiEvent()
-    object hideDialog : UsuarioUiEvent()
-    object userMessageShown : UsuarioUiEvent()
-    data class Login(val userName: String, val password: String) : UsuarioUiEvent()
-    object ToggleLoginMode: UsuarioUiEvent()
+import android.content.Context
 
+sealed class UsuarioUiEvent {
+    data class UserNameChanged(val value: String) : UsuarioUiEvent()
+    data class PasswordChanged(val value: String) : UsuarioUiEvent()
+    data class ConfirmPasswordChanged(val value: String) : UsuarioUiEvent()
+    data class Login(val user: String, val pass: String) : UsuarioUiEvent()
+    data class RememberMeChanged(val isChecked: Boolean) : UsuarioUiEvent()
+    data class OnRecoveryEmailChanged(val value: String) : UsuarioUiEvent()
+    data class OnSendRecoveryEmail(val context: Context) : UsuarioUiEvent()
+
+    object Save : UsuarioUiEvent()
+    object ToggleLoginMode : UsuarioUiEvent()
+    object OnForgotPasswordClicked : UsuarioUiEvent()
+    object OnRecoveryDialogDismissed : UsuarioUiEvent()
+    object HideDialog : UsuarioUiEvent()
+    object UserMessageShown : UsuarioUiEvent()
 }
