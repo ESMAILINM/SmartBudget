eac68dac81ca49386875c1fb94e8b7bdbfbbe6c5
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-10 16:29:58 -0400

    feat(MetaUiState): ✨ agregar clase de estado UI para gestionar la información de las metas

---
+++ app/src/main/java/edu/ucne/smartbudget/presentation/meta/MetaScreen/MetaUiState.kt
@@ -0,0 +1,21 @@
+package edu.ucne.smartbudget.presentation.meta.MetaScreen
+
+data class MetaUiState(
+    val metaId: String? = null,
+    val nombre: String = "",
+    val monto: String = "",
+    val contribucionMensual: String = "",
+    val fecha: String = "",
+    val emoji: String = "",
+    val imagenes: List<String> = emptyList(),
+    val selectedImageIndex: Int = 0,
+    val selectedImageUri: String? = null,
+    val isLoading: Boolean = false,
+    val isSaving: Boolean = false,
+    val isSuccess: Boolean = false,
+    val errorMessage: String? = null,
+    val isNew: Boolean = true,
+    val deleted: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val montoAhorrado : Double = 0.0 
+)
