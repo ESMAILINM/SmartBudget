ef8f94c77c31bc3ca65b5c493ee9b9f016edcdaa
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-10 13:06:28 -0400

    refactor(usuario): ♻️ actualizar UsuarioUiState para mejorar manejo de estados y agregar nuevos campos

--- app/src/main/java/edu/ucne/smartbudget/presentation/Usuarios/UsuarioUiState.kt
+++ app/src/main/java/edu/ucne/smartbudget/presentation/Usuarios/UsuarioUiState.kt
@@ -2,9 +2,10 @@ package edu.ucne.smartbudget.presentation.Usuarios
 
 import edu.ucne.smartbudget.domain.model.Usuarios
 
-data class UsuarioUiState (
-    val usuarioId: Int = 0,
+data class UsuarioUiState(
+    val usuarioId: String = "",
     val userName: String = "",
+    val load: Boolean = false,
     val password: String = "",
     val isNew: Boolean = true,
     val isSaving: Boolean = false,
@@ -16,9 +17,15 @@ data class UsuarioUiState (
     val showDialog: Boolean = false,
     val userMessage: String? = null,
     val successLogin: Boolean = false,
+    val confirmPassword: String = "",
+    val userNameError: String? = null,
+    val passwordError: String? = null,
+    val confirmPasswordError: String? = null,
     val loginError: String? = null,
     val usuarios: List<Usuarios> = emptyList(),
     val usuarioActual: Usuarios? = null,
-    val isLogin: Boolean = true
-
+    val isLogin: Boolean = true,
+    val rememberMe: Boolean = false,
+    val showRecoveryDialog: Boolean = false,
+    val recoveryEmail: String = ""
 )
