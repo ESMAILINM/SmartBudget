15e892dea663a211130e4c45f829e8f2197f1b34
ESMAILINM <esmailinmartinez05@gmail.com>
2025-12-10 13:36:44 -0400

    feat(categoria): ✨ agregar clase de datos CategoryProgress para representar el progreso de las categorías

---
+++ app/src/main/java/edu/ucne/smartbudget/presentation/dashboardScreen/components/items/CategoryProgress.kt
@@ -0,0 +1,8 @@
+package edu.ucne.smartbudget.presentation.dashboardScreen.components.items
+
+
+data class CategoryProgress(
+    val categoria: String,
+    val monto: Double,
+    val porcentaje: Double
+)
